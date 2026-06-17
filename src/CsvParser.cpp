fe2870a754d963f5656d17f14aec5caf48689f1e
AngelCruz <angeld.cruz@outlook.com>
2026-06-16 20:18:54 -0600

    Succesfully parse information into a vector that contains the required data structs adn receive it at main

--- src/CsvParser.cpp
+++ src/CsvParser.cpp
@@ -16,34 +16,46 @@ std::vector<MarketData::OrderEvent> MarketData::parseCsvFile(std::string filePat
     std::string line;
     int cont=1;
 
-    while(getline(file, line)&&cont<5){
-
+    while(getline(file, line)&&cont<=5){
+        MarketData::OrderEvent TEvent;
         int column = 0;
         std::stringstream information(line);
         std::string token;
         while(getline(information, token, ',')){
-            switch(static_cast<MarketData::OrderColumn>(column)){
+
+            switch(MarketData::OrderColumn(column)){
                 case MarketData::OrderColumn::TIMESTAMP :
-                std::cout << "This is the amount of time (up to microseconds) of the purchase after 12am: " << token << "\n";  
+                //std::cout << "This is the amount of time (up to microseconds) of the purchase after 12am: " << token << "\n";
+                TEvent.timestamp=std::stod(token);
                 break;
                 case MarketData::OrderColumn::EVENT : 
-                std::cout << "This is the type of event: " << token << "\n"; 
+                //std::cout << "This is the type of event: " << token << "\n"; 
+                TEvent.eventType = std::stoi(token);
                 break;
                 case MarketData::OrderColumn::ID :
-                std::cout << "This is its ID: " << token << "\n"; 
+                //std::cout << "This is its ID: " << token << "\n"; 
+                TEvent.orderId = std::stoll(token);
                 break;
                 case MarketData::OrderColumn::AMOUNT :
-                std::cout << "This is the amount of this stock available: " << token << "\n"; 
+                //std::cout << "This is the amount of this stock available: " << token << "\n"; 
+                TEvent.amount = std::stoi(token);
                 break;
                 case MarketData::OrderColumn::PRICE :
-                std::cout << "This is the price per stock: " << token << "\n"; 
+                //std::cout << "This is the price per stock: " << token << "\n"; 
+                TEvent.price = std::stod(token);
                 break;
                 case MarketData::OrderColumn::DIRECTION :
-                std::cout << "This is the direciton of this event: " << token << "\n"; 
+                //std::cout << "This is the direciton of this event: " << token << "\n"; 
+                if(token=="1"){
+                    TEvent.direction = OrderDirection::Buy;
+                } else {
+                    TEvent.direction = OrderDirection::Sell;
+                }
                 break;
             }
             column++;
         }
+        Info.push_back(TEvent);
         cont++;
     }
 
--- src/main.cpp
+++ src/main.cpp
@@ -2,5 +2,16 @@
 #include <iostream>
 
 int main(){
-    MarketData::parseCsvFile("../data/OrderBookActions_APPLE.csv");
+
+    std::vector<MarketData::OrderEvent> Events = MarketData::parseCsvFile("../data/OrderBookActions_APPLE.csv");
+    if(Events.empty()){
+        std::cout << "Couldn't register any events.\n";
+        exit(1);
+    }
+
+    for(int i = 0; i < 5; i++){
+        MarketData::OrderEvent temp = Events[i];
+        std::cout << "At time " << temp.timestamp << ", there have been "<< temp.amount << " stocks updated with ID: "<< temp.orderId << "\n";
+    }
+
 }
